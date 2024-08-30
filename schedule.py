c7a071dfcf16de7afeb318e2b2d3bfb8071633eb
sethlagman <sethglagman@gmail.com>
2024-08-31 03:30:20 +0800

    Updated function names and added new ones

--- schedule.py
+++ schedule.py
@@ -3,25 +3,49 @@
 import requests
 import json
 
+URL = 'https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_1.json'
+
 class NbaSchedule:
     """Retrieves the schedule of NBA games"""
 
-    def __init__(self) -> None:
+    def __init__(self, filename='nbaschedule.json', url=URL) -> None:
         """Initialize attributes"""
 
-        self.url = 'https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_1.json'
+        self.url = url
         self.response = requests.get(self.url)
-        self.result = None
+        self.filename = filename
+        self.schedule = self.read_schedule()
 
-    def fetch_schedule(self):
-        """Fetches the game schedule"""
-        
+    def fetch_shedule(self):
+        """Fetches the game schedules for the whole season"""
+
+        season_year = self.schedule['leagueSchedule']['seasonYear']
+        games = self.schedule['leagueSchedule']['gameDates']
+        for game in games[:2]:
+            print(f'Date: {game['gameDate'][:10]}')
+            for sched in game['games']:
+                print(f'Game: {sched['gameCode']}')
+                print(f'Home: {sched['homeTeam']['teamCity']} {sched['homeTeam']['teamName']}')
+                print(f'Away: {sched['awayTeam']['teamCity']} {sched['awayTeam']['teamName']}')
+                print()
+            print('------')
+
+    def fetch_date_schedule(self):
+        """Fetches the game schedules for the given date"""
+
         pass
+
+    def read_schedule(self):
+        """Reads the game schedules from the stored json file"""
+        
+        with open(self.filename, 'r') as file:
+            return json.load(file)
 
     def store_schedule(self):
-        """Stores the game schedule"""
+        """Stores the game schedules as a json file"""
 
-        pass
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(self.response.json(), indent=2))
 
     def update_schedule(self):
         """Updates the game schedule"""
@@ -31,7 +55,8 @@ class NbaSchedule:
 
 def main():
 
-    pass
+    nbaschedule = NbaSchedule()
+    nbaschedule.fetch_shedule()
 
 if __name__ == '__main__':
     main()
