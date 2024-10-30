3f4ab85399d6e213881c7e36274f43efa4a36dda
seth <sethglagman@gmail.com>
2024-10-31 00:54:03 +0800

    Create class for handling files and endpoints

---
+++ filehandler.py
@@ -0,0 +1,37 @@
+"""File Handler"""
+
+import requests
+import json
+
+class FileHandler:
+    """Handles files"""
+
+    def __init__(self, endpoint):
+        """Initialize attributes"""
+
+        self.endpoint = endpoint
+        self.response = requests.get(json.load(open('endpoints.json'))['endpoints'][endpoint])
+
+    def store(self):
+        """Stores the file"""
+
+        with open(self.endpoint + '.json', 'w') as file:
+            file.write(json.dumps(self.response.json(), indent=2))
+
+    def read(self):
+        """Reads the file"""
+
+        with open(self.endpoint + '.json', 'r') as file:
+            return json.load(file)
+
+    def update(self):
+        """Regularly updates the file given a time interval"""
+
+        pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
