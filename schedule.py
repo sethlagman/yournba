4df4a2b24a085bd07a811a5a97bb00e92cc5c984
seth <sethglagman@gmail.com>
2024-10-31 00:56:07 +0800

    Modify NbaSchedule class

--- schedule.py
+++ schedule.py
@@ -1,24 +1,20 @@
 """NBA Schedule"""
 
-import requests
 import json
 
-URL = 'https://cdn.nba.com/static/json/staticData/scheduleLeagueV2_1.json'
+from filehandler import FileHandler
 
 class NbaSchedule:
     """Retrieves the schedule of NBA games"""
 
-    def __init__(self, filename='nbaschedule.json', url=URL) -> None:
+    def __init__(self):
         """Initialize attributes"""
 
-        self.url = url
-        self.response = requests.get(self.url)
-        self.filename = filename
-        self.schedule = self.read_schedule()
+        self.schedule = FileHandler('schedule').read()
 
     def fetch_shedule(self):
         """Fetches the game schedules for the whole season"""
-
+        
         return {
             games['gameDate'][:10]: [
                 {
@@ -43,28 +39,10 @@ class NbaSchedule:
 
         pass
 
-    def read_schedule(self):
-        """Reads the game schedules from the stored json file"""
-        
-        with open(self.filename, 'r') as file:
-            return json.load(file)
-
-    def store_schedule(self):
-        """Stores the game schedules as a json file"""
-
-        with open(self.filename, 'w') as file:
-            file.write(json.dumps(self.response.json(), indent=2))
-
-    def update_schedule(self):
-        """Updates the game schedule"""
-
-        pass
-
 
 def main():
 
-    nbaschedule = NbaSchedule()
-    print(json.dumps(nbaschedule.fetch_shedule(), indent=2))
+    pass
 
 if __name__ == '__main__':
     main()
