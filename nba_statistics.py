9c3fad21f7e7bd6e4387efc0b7f2455919c9ccc3
sethlagman <sethglagman@gmail.com>
2024-11-29 22:33:51 +0800

    Foundation for nba statistics

--- nba_statistics.py
+++ nba_statistics.py
@@ -1,6 +1,33 @@
-"""NBA Players' Statistics"""
+"""NBA Statistics"""
 
-class Statistics:
+import json
+
+from filehandler import FileHandler
+
+class NbaStatistics:
     """
     Retrieves NBA players' statistics
     """
+
+    def __init__(self) -> None:
+        
+        self.statistics = FileHandler('statistics').read()
+
+    def fetch_statistics(self) -> dict:
+        """
+        Retrieves all the players' statistics for the current season
+
+        Returns:
+            dict: - Statistics of the players
+        """
+
+        for player in self.statistics['payload']['players']:
+            print(json.dumps(player, indent=2))
+
+
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
