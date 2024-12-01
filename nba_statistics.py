0db496fee2baf35d79c4a4c1fc3e7e51843a145b
sethlagman <sethglagman@gmail.com>
2024-12-01 23:26:18 +0800

    Add nba_statistics.py
    For working with the scraped nba dataset about player statistics

---
+++ nba_statistics.py
@@ -0,0 +1,34 @@
+"""NBA Statistics"""
+
+from filehandler import FileHandler
+
+class NbaStatistics:
+    """
+    Retrieves NBA players' statistics
+    """
+
+    def __init__(self) -> None:
+        """Initialize attributes"""
+
+        self.statistics = FileHandler().read(filename='nba_scraper\statistics.json')
+
+    def fetch_player_statistics(self, first_name: str, last_name: str) -> dict:
+        """
+        Retrieves the given player's statistics
+        Args:
+            first_name (string): - First name of the player
+            last_name (string): - Last name of the player
+        """
+
+        formatted_name = f'{first_name} {last_name}'.lower()
+        for statistic in self.statistics:
+            if statistic['name'].lower() == formatted_name:
+                return statistic
+
+
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
