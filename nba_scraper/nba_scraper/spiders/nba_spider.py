a5991d5c667d98c0e152e3df45ccc8cedf98a728
sethlagman <sethglagman@gmail.com>
2024-12-01 22:46:59 +0800

    Add scraper for scraping nba statistics

--- nba_scraper/nba_scraper/spiders/nba_spider.py
+++ nba_scraper/nba_scraper/spiders/nba_spider.py
@@ -2,7 +2,37 @@ import scrapy
 
 class NbaScraperSpider(scrapy.Spider):
     name = 'nbascraper'
-    starturls = ['https://www.nba.com/stats/players/traditional?PerMode=PerGame&sort=PTS&dir=-1']
+    start_urls = ['https://www.nbastuffer.com/2024-2025-nba-player-stats/']
 
     def parse(self, response):
-        pass
+        rows = response.css('tbody.row-hover tr')
+        for row in rows:
+            data = {
+                'name': row.css('.column-2::text').get(),  # Get the player's name
+                'team': row.css('.column-3::text').get(),  # Get the player's team
+                'position': row.css('.column-4::text').get(),  # Get the player's position
+                'age': row.css('.column-5::text').get(),  # Get the player's age
+                'gp': row.css('.column-6::text').get(),  # Get the player's games played
+                'mpg': row.css('.column-7::text').get(),  # Get the player's minutes per game
+                'usg': row.css('.column-8::text').get(),  # Get the player's usage rate
+                'to': row.css('.column-9::text').get(),  # Get the player's turnovers
+                'fta': row.css('.column-10::text').get(),  # Get the player's free throw attempts
+                'ft': row.css('.column-11::text').get(),  # Get the player's free throw percentage
+                'twopa': row.css('.column-12::text').get(),  # Get the player's 2P%
+                'twop': row.css('.column-13::text').get(),  # Get the player's 2P made
+                'threepa': row.css('.column-14::text').get(),  # Get the player's 3P attempts
+                'threep': row.css('.column-15::text').get(),  # Get the player's 3P made
+                'efg': row.css('.column-16::text').get(),  # Get the player's effective field goal percentage
+                'ts': row.css('.column-17::text').get(),  # Get the player's true shooting percentage
+                'ppg': row.css('.column-18::text').get(),  # Get the player's points per game
+                'apg': row.css('.column-19::text').get(),  # Get the player's assists per game
+                'spg': row.css('.column-20::text').get(),  # Get the player's steals per game
+                'bpg': row.css('.column-21::text').get(),  # Get the player's blocks per game
+                'tpg': row.css('.column-22::text').get(),  # Get the player's turnovers per game
+                'pplusr': row.css('.column-23::text').get(),  # Get the player's points per rebound
+                'pplusa': row.css('.column-24::text').get(),  # Get the player's points per assist
+                'pplusrplusa': row.css('.column-25::text').get(),  # Get the player's points per rebound + assist
+                'vi': row.css('.column-26::text').get(),  # Get the player's value index
+                'ortg': row.css('.column-27::text').get(),  # Get the player's offensive rating
+            }
+            yield data
