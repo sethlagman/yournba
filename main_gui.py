7d7285bdbbc0e2075a454d045a39239f035a0b4b
sethlagman <sethglagman@gmail.com>
2024-12-03 23:02:18 +0800

    Add frames for better widget organization

--- main_gui.py
+++ main_gui.py
@@ -9,14 +9,30 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
 from modules.nba_schedule import *
 from modules.nba_statistics import *
 
+class EntryFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, height=50, width=300, fg_color='transparent')
+        self.grid_propagate(0)
+
+        self.grid_columnconfigure(0, weight=5)
+        self.grid_columnconfigure(1, weight=1)
+
+        search = ctk.CTkEntry(self, placeholder_text='Player, game id, schedule date, and etc.', height=30)
+        search.grid(row=1, column=0, sticky='ew', padx=(20, 5))
+
+        search_btn = ctk.CTkButton(self, text='Search', height=30)
+        search_btn.grid(row=1,column=1, sticky='ew', padx=(5, 20))
+
+
 class OutputFrame(ctk.CTkScrollableFrame):
     def __init__(self, master):
         super().__init__(master, height=500, width=300)
-        self.toplevel_window = None
         
+        self.grid_columnconfigure(0, weight=1)
+
         current_date = f'{d.today().month}/0{d.today().day}/{d.today().year}'
         schedules = NbaSchedule().fetch_shedule()
-
+        
         for schedule in schedules[150:200]:
             for date, games in schedule.items():
                 date_label = ctk.CTkLabel(self, text=date)
@@ -25,37 +41,35 @@ class OutputFrame(ctk.CTkScrollableFrame):
                     matchup = f'{game['Home']} vs {game['Away']}'
                     matchup_label = ctk.CTkLabel(self, text=matchup)
                     matchup_label.grid()
-            
+
+
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.toplevel_window = None
-        #self.protocol("WM_DELETE_WINDOW", self.exit_app)
-
-        self.geometry("500x650")
         self.title('')
+        self.geometry("500x650")
         self.iconbitmap(r'images\nbalogo.ico')
-        self.grid_columnconfigure(0, weight=20)
-        self.grid_columnconfigure(1, weight=1)
+        self.protocol('WM_DELETE_WINDOW', self.exit_app)
+        
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=1)
-        
+        self.grid_columnconfigure(0, weight=1)
+
         title = ctk.CTkLabel(self, text="YourNBA", font=('', 50, 'bold'))
-        title.grid(pady=(20, 15), columnspan=2)
+        title.grid(row=0, pady=(20, 15))
 
-        search = ctk.CTkEntry(self, placeholder_text='Player, game id, schedule date, and etc.', height=30)
-        search.grid(row=1, column=0, sticky='ew', padx=(20, 0))
+        entry_frame = EntryFrame(self)
+        entry_frame.grid(row=1, sticky='new')
 
-        search_btn = ctk.CTkButton(self, text='Search', height=30)
-        search_btn.grid(row=1, column=1, sticky='e', padx=(10, 20))
-        
-        self.output_frame = OutputFrame(self)
-        self.output_frame.grid(row=2, columnspan=2, sticky='ew', pady=15, padx=15)
+        output_frame = OutputFrame(self)
+        output_frame.grid(row=2, sticky='ew', padx=(10, 10), pady=(0, 10))
 
     def exit_app(self):
         confirm = messagebox.askyesno('', 'Are you sure you want to close the application?')
         if confirm:
             self.quit()
-        
+
 
 app = App()
 app.mainloop()
