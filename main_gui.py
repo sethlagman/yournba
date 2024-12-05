c9dc82d0acbf678374163750c1bf6725a071df4c
sethlagman <sethglagman@gmail.com>
2024-12-06 00:46:31 +0800

    Add sidebar and refactored code

--- main_gui.py
+++ main_gui.py
@@ -9,9 +9,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
 from modules.nba_schedule import *
 from modules.nba_statistics import *
 
+class SideBarFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master, width=250)
+        self.grid(column=0, row=0, rowspan=3, sticky='nsew', padx=(10, 0), pady=(10, 10))
+        self.grid_propagate(0)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        title = ctk.CTkLabel(self, text='Menu', font=('', 30, 'bold'))
+        title.grid(column=0, row=0, sticky='n', pady=(10, 15))
+
+
 class EntryFrame(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(master, height=50, width=300, fg_color='transparent')
+        super().__init__(master, fg_color='transparent')
+        self.grid(column=1, row=1, sticky='new')
         self.grid_propagate(0)
 
         self.grid_columnconfigure(0, weight=5)
@@ -26,8 +40,8 @@ class EntryFrame(ctk.CTkFrame):
 
 class OutputFrame(ctk.CTkScrollableFrame):
     def __init__(self, master):
-        super().__init__(master, height=500, width=300)
-        
+        super().__init__(master, height=650, width=545)
+        self.grid(column=1, row=2, sticky='ew', padx=(10, 10), pady=(10, 10))
         self.grid_columnconfigure(0, weight=1)
 
         current_date = f'{d.today().month}/0{d.today().day}/{d.today().year}'
@@ -46,24 +60,22 @@ class OutputFrame(ctk.CTkScrollableFrame):
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title('')
-        self.geometry("500x650")
+        self.geometry("850x650")
         self.iconbitmap(r'images\nbalogo.ico')
+        self.resizable(False, False)
+        self.title('')
         self.protocol('WM_DELETE_WINDOW', self.exit_app)
         
         self.grid_rowconfigure(0, weight=0)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure((1, 2), weight=1)
+        self.grid_columnconfigure((0, 1), weight=0)
 
         title = ctk.CTkLabel(self, text="YourNBA", font=('', 50, 'bold'))
-        title.grid(row=0, pady=(20, 15))
+        title.grid(column=1, row=0, pady=(20, 15))
 
-        entry_frame = EntryFrame(self)
-        entry_frame.grid(row=1, sticky='new')
-
-        output_frame = OutputFrame(self)
-        output_frame.grid(row=2, sticky='ew', padx=(10, 10), pady=(0, 10))
+        sidebar = SideBarFrame(self)
+        entry = EntryFrame(self)
+        output = OutputFrame(self)
 
     def exit_app(self):
         confirm = messagebox.askyesno('', 'Are you sure you want to close the application?')
